bd656846f94467b958010611f9399b64d401b932
Felipe Assis <felipeassis97@gmail.com>
2024-01-07 08:52:56 -0300

    feat: filter by rate stores

--- ChefDelivery/ChefDelivery/StoresView/StoresListView.swift
+++ ChefDelivery/ChefDelivery/StoresView/StoresListView.swift
@@ -8,29 +8,74 @@
 import SwiftUI
 
 struct StoresListView: View {
+    
+    @State private var rateFilter = 0
+    var filteredStores: [StoreType] {
+        return storesMock.filter { store in
+            store.rate >= rateFilter
+        }
+    }
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 4) {
-            Text("Lojas")
-                .font(.headline)
-                .fontWeight(.semibold)
-                .padding(.leading, 16)
-                .padding(.bottom, 8)
-
-            List(storesMock) {
-                store in
+        VStack(spacing: 4) {
+            HStack {
+                Text("Lojas")
+                    .font(.headline)
+                    .fontWeight(.semibold)
+                    .padding(.leading, 16)
+                    .padding(.bottom, 8)
                 
-                NavigationLink(destination: {
-                    StoreDetailView(store: store)
-                }, label: {
-                    StoreItemView(orderItem: store)
-                })
+                Spacer()
+            
+                Menu("Filtrar") {
+                    Button(action: {
+                        rateFilter = 0
+                    }, label: {
+                        Text("Limpar Filtro")
+                    })
+                    Divider()
+                    ForEach(1...5, id: \.self) { rate in
+                        Button(action: {
+                            rateFilter = rate
+                        }, label: {
+                            rate > 1 ?
+                            Text("\(rate) estrelas ou mais") :
+                            Text("\(rate) estrela ou mais")
+                        })
+                    }
+                }
+                .foregroundStyle(.black)
+                .padding(.trailing)
+                
             }
-            .scrollDisabled(true)
-            .scrollContentBackground(.hidden)
-            .scaledToFit()
-            .listStyle(.plain)
-            .foregroundStyle(.black)
-            .listRowSeparator(.hidden)
+           
+            if(filteredStores.isEmpty) {
+                VStack(alignment: .center, spacing: 16) {
+                    Image("EmptyState")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .padding(.top, 32)
+                    
+                    Text("Nenhma loja encontrada")
+                }
+            } else {
+                List(filteredStores) { store in
+                    NavigationLink(destination: {
+                        StoreDetailView(store: store)
+                    }, label: {
+                        StoreItemView(orderItem: store)
+                    })
+                }
+                .scrollDisabled(true)
+                .scrollContentBackground(.hidden)
+                .scaledToFit()
+                .listStyle(.plain)
+                .foregroundStyle(.black)
+                .listRowSeparator(.hidden)
+            }
+            
+       
 
         }
        
