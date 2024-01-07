ea1fa3a77ea7c4cbb254f644ab3859cfe77f18d2
Felipe Assis <felipeassis97@gmail.com>
2024-01-07 10:20:05 -0300

    chore: Improvements banner transitions

--- ChefDelivery/ChefDelivery/CarrouselView/CarrousselTabView.swift
+++ ChefDelivery/ChefDelivery/CarrouselView/CarrousselTabView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct CarrousselTabView: View {
+    @State private var currentCarrousselIndex = 1
+    
     var body: some View {
-        TabView {
+        TabView(selection: $currentCarrousselIndex) {
             ForEach(banners) {
                 item in
                 CarrousselItemView(orderItem: item)
+                    .tag(item.id)
             }
         }
         .tabViewStyle(.page(indexDisplayMode: .always))
         .frame(height: 180)
-        
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+                withAnimation(.easeInOut(duration: 1)) {
+                    if currentCarrousselIndex > banners.count {
+                        currentCarrousselIndex = 1
+                    }
+                    currentCarrousselIndex += 1
+                }
+            }
+        }
     }
 }
 
